458db2f3661c6d41a6d2101bb86da9ca729950e1
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2022-03-05 11:08:44 +0100

    locales folder > en/fr files created, data.json added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { createI18n } from 'vue-i18n'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -10,9 +11,35 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, fab)
 
+
+
+const messages = {
+	en: {
+		message: {
+			hello: "hello world"
+		}
+	},
+	fr: {
+		message: {
+			hello: "bonjour monde"
+		}
+	}
+}
+
+
+
+const i18n = createI18n({
+	locale: 'en', // set locale
+  fallbackLocale: 'fr', // set fallback locale
+  messages, // set locale messages
+})
+
+
 const app = createApp(App)
 
 app.component('fa', FontAwesomeIcon)
+
+app.use(i18n)
 app.use(router)
 
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,18 @@
 		<Header />
 	</transition>
 
+
+
+
+
+
+	<p>{{ $t("message.hello") }}</p>
+
+
+
+
+
+
 	<transition name="fade" appear>
 		<main class="grid place-items-center mt-24 space-y-10">
 			<Card @click="setBackground" v-for="(project, index) in projects" :key="index"
@@ -39,6 +51,9 @@ export default {
   },
 	data() {
 		return {
+			
+			// add img alt
+
 			projects: [
 				{
 					id:1,
