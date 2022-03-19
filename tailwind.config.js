91a861d358e6d88a5be4601d93df428879c35e9a
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2022-03-19 11:02:07 +0100

    purge parameters removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@ module.exports = {
   content: [
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
-
-  purge: [
-    "./views/*.vue", 
-    "./components/*.vue"
-  ],
-
-
   theme: {
     extend: {
 			colors: {
