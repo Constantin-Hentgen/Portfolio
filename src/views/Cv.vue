5bf48e5d3640c2b051d136121e01e2fe4e1a3842
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2022-08-22 12:31:35 +0200

    pdf source added

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -2,7 +2,7 @@
   <div>
 		<Header />
 		<div class="w-full grid place-items-center">
-			<iframe src="https://www.irs.gov/pub/irs-pdf/fw3.pdf" height="1000px" width="500px" class="w-11/12 xl:w-1/2 rounded-md mt-10"></iframe>
+			<iframe src="https://constantin-hentgen.fr/static/cv.pdf" height="1000px" width="500px" class="w-11/12 xl:w-1/2 rounded-md mt-10"></iframe>
 		</div>
 	</div>
 </template>
