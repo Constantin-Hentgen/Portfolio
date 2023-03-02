faf2eef365e3d162ec55f9d1b58488169ad2cc3c
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2023-03-02 22:36:42 +0100

    menu amélioré

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,48 @@
 <template>
-	<header id="header" class="w-full flex-col text-center p-5 fixed gap-10">
-		<button v-on:click="toggleNav()" class="text-myBlue-200 bg-myBlue-900 text-3xl mb-16 w-20 h-20 border-myBlue-200 border-2 rounded-full">
-			<i v-if="!navOpen" class="fa fa-bars"></i>
-			<i v-else class="fas fa-xmark"></i>
-		</button>
+	<header id="header" class="w-full flex-col p-5 fixed">
+		<div class="w-full h-20 grid place-items-end">
+			<button v-on:click="toggleNav()" class="text-myBlue-200 bg-myBlue-900 text-3xl mb-16 w-20 h-20 shadow-xl border-myBlue-200 border-2 rounded-full">
+				<i v-if="!navOpen" class="fa fa-bars"></i>
+				<i v-else class="fas fa-xmark"></i>
+			</button>
+		</div>
 
-		<nav class="hidden w-full h-full flex-col gap-5" id="nav">
-			<router-link :to="`/${$i18n.locale}`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.home")}} </router-link>
-			<router-link :to="`/${$i18n.locale}/ambitions`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.professional")}} </router-link>
-			<router-link :to="`/${$i18n.locale}/experiences`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.experience")}} </router-link>
-			<router-link :to="`/${$i18n.locale}/commitment`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.commitment")}} </router-link>
-			<router-link :to="`/${$i18n.locale}/projects`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.projects")}} </router-link>
+		
+		<nav class="hidden w-full h-full flex-col gap-10 p-12" id="nav">
+			<h1 class="text-4xl font-bold">Go to...</h1>
+
+			<ul>
+				<li class="mt-5">
+					<router-link :to="`/${$i18n.locale}`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> 
+						<i class="fa fa-house"></i>
+						{{$t("landing-page.nav.home")}} 
+					</router-link>
+				</li>
+				<li class="mt-5">
+					<router-link :to="`/${$i18n.locale}/experiences`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> 
+						<i class="fa fa-suitcase"></i>
+						{{$t("landing-page.nav.experience")}} 
+					</router-link>
+				</li>
+				<li class="mt-5">
+					<router-link :to="`/${$i18n.locale}/projects`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> 
+						<i class="fa-solid fa-vial"></i>
+						{{$t("landing-page.nav.projects")}} 
+					</router-link>
+				</li>
+				<li class="mt-5">
+					<router-link :to="`/${$i18n.locale}/ambitions`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> 
+						<i class="fa fa-bullseye"></i>
+						{{$t("landing-page.nav.professional")}} 
+					</router-link>
+				</li>
+				<li class="mt-5">
+					<router-link :to="`/${$i18n.locale}/commitment`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> 
+						<i class="fa-solid fa-people-group"></i>
+						{{$t("landing-page.nav.commitment")}} 
+					</router-link>
+				</li>
+			</ul>
 		</nav>
 	</header>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
 		space-y-5
 		md:space-x-10
 	">
-		<img class="w-40 h-40 sm:w-48 mt-32 sm:h-48 mx-auto md:w-60 md:h-60 shadow-2xl rounded-full" src="../assets/pp.jpeg" alt="picture of the webmaster">
+		<img class="w-40 h-40 sm:w-48 mt-20 sm:h-48 mx-auto md:w-60 md:h-60 shadow-2xl rounded-full" src="../assets/pp.jpeg" alt="picture of the webmaster">
 		<h1 class="text-3xl  md:text-4xl text-center md:text-left text-myBlue-900 font-extrabold">Constantin, {{ $t('landing-page.student') }} <i class="fas fa-shield-halved text-2xl md:text-3xl"></i></h1>
 		<p class="text-left p-5 w-full text-md"> {{ $t('landing-page.description') }} </p>
 	</div>
