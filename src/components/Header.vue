7bcc4626ff8a0c2318e2c040feae1c72c08d1c95
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2023-03-02 17:48:31 +0100

    reprise musclée

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,18 @@
 <template>
-  <div>
-		<header id="xprime" class="bg-myBlue-900 z-10 w-full shadow-md fixed top-0">
-			<div class="topnav w-full bg-myBlue-900 md:flex md:justify-center  pl-10 md:pl-0 text-myBlue-200 font-semibold" id="myTopnav">
-				<a class="icon" v-on:click="toggleNav()">
-					<i v-if="!navOpen" class="fa fa-bars"></i>
-					<i v-else class="fas fa-xmark text-xl"></i>
-				</a>
+	<header id="header" class="w-full flex-col text-center p-5 fixed bg-myBlue-200 gap-10 mb-10">
+		<button v-on:click="toggleNav()" class="text-myBlue-200 bg-myBlue-900 text-3xl w-20 h-20 rounded-full">
+			<i v-if="!navOpen" class="fa fa-bars"></i>
+			<i v-else class="fas fa-xmark"></i>
+		</button>
 
-				<router-link :to="`/${$i18n.locale}`" id="info"> Constantin Hentgen </router-link>
-
-				<router-link :to="`/${$i18n.locale}`" v-on:click.native="toggleNav()" class="hover:bg-white w-4/5 rounded-lg md:rounded-none md:w-full hover:text-myBlue-900 text-3xl focus:bg-white focus:text-myBlue-900"> {{$t("landing-page.nav.home")}} </router-link>
-				<router-link :to="`/${$i18n.locale}/ambitions`" v-on:click.native="toggleNav()" class="hover:bg-white w-4/5 rounded-lg md:rounded-none md:w-full hover:text-myBlue-900 text-3xl focus:bg-white focus:text-myBlue-900"> {{$t("landing-page.nav.professional")}} </router-link>
-				<router-link :to="`/${$i18n.locale}/experiences`" v-on:click.native="toggleNav()" class="hover:bg-white w-4/5 rounded-lg md:rounded-none md:w-full hover:text-myBlue-900 text-3xl focus:bg-white focus:text-myBlue-900"> {{$t("landing-page.nav.experience")}} </router-link>
-				<router-link :to="`/${$i18n.locale}/commitment`" v-on:click.native="toggleNav()" class="hover:bg-white w-4/5 rounded-lg md:rounded-none md:w-full hover:text-myBlue-900 text-3xl focus:bg-white focus:text-myBlue-900"> {{$t("landing-page.nav.commitment")}} </router-link>
-				<router-link :to="`/${$i18n.locale}/projects`" v-on:click.native="toggleNav()" class="hover:bg-white w-4/5 rounded-lg md:rounded-none md:w-full hover:text-myBlue-900 text-3xl focus:bg-white focus:text-myBlue-900"> {{$t("landing-page.nav.projects")}} </router-link>
-			</div>
-		</header>
-  </div>
+		<nav class="hidden w-full h-full flex-col gap-5" id="nav">
+			<router-link :to="`/${$i18n.locale}`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.home")}} </router-link>
+			<router-link :to="`/${$i18n.locale}/ambitions`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.professional")}} </router-link>
+			<router-link :to="`/${$i18n.locale}/experiences`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.experience")}} </router-link>
+			<router-link :to="`/${$i18n.locale}/commitment`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.commitment")}} </router-link>
+			<router-link :to="`/${$i18n.locale}/projects`" v-on:click.native="toggleNav()" class="text-myBlue-900 text-3xl"> {{$t("landing-page.nav.projects")}} </router-link>
+		</nav>
+	</header>
 </template>
 
 <script>
@@ -29,127 +25,19 @@ export default {
 	},
 	methods: {
 		toggleNav() {
-			var x = document.getElementById("myTopnav");
-			var xprime = document.getElementById("xprime");
-			var y = document.getElementById("info");
-
-			if (!x.className.includes("responsive")) {
+			var header = document.getElementById("header");
+			var nav = document.getElementById("nav");
+			if (!this.navOpen) {
+				header.style.height = window.innerHeight + "px";
+				nav.style.display = "flex";
 				this.navOpen = true;
-				if (window.innerWidth < 700) {
-					xprime.animate(
-						[
-							{ transform: 'translateY(-400px)' },
-							{ transform: 'translateY(0px)' }
-						],
-						{
-							duration: 225
-						}
-					);
-					content.style.filter = "blur(3px)";
-					x.className += " responsive";
-				}
-				
-				y.style.display = "none";
 			} else {
+				header.style.height = "50px";
+				nav.style.display = "none";
 				this.navOpen = false;
-				content.style.filter = "blur(0px)";
-
-				x.animate(
-					[
-						{ transform: 'translateX(-500px)'},
-						{ transform: 'translateY(0)' }
-					],
-					{
-						duration: 200
-					}
-				);
-
-				// le nouveau afit 400px de trop et il se fait resize
-
-				x.className = "topnav w-full bg-myBlue-900  pl-10 text-white font-semibold";
-				y.style.display = "block";
 			}
 		}
 	}
 }
 </script>
-
-<style>
-* {
-	-webkit-tap-highlight-color: transparent;
-}
-
-.dropdown-fade-enter-active, .dropdown-fade-leave-active {
-	transition: all .1s ease-in-out;
-}
-.dropdown-fade-enter, .dropdown-fade-leave-to {
-	opacity: 0;
-	transform: translateY(-12px);
-}
-
-/* NEW TOP NAV */
-
-.topnav {
-  overflow: hidden;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-#info {
-	display: none;
-}
-
-.topnav .icon, .topnav .info {
-  display: none;
-}
-
-@media screen and (max-width: 750px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav .info {
-    float: left;
-    display: block;
-  }
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-
-  .topnav.responsive {
-		position: relative;
-	}
-
-	#info {
-		display: block;
-	}
-
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
-
-@media screen and (min-width: 751px) {
-	#myTopnav {
-		display: flex;
-		justify-content: center;
-	}
-	#info {
-		display: none !important;
-	}
-}
-</style>
 
